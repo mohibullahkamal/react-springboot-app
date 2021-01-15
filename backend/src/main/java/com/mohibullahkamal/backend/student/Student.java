7aa86be76da8afbcecc7f13d86e617f57f21c955
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-15 16:56:29 +0100

    build student model

---
+++ backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
@@ -0,0 +1,8 @@
+package com.mohibullahkamal.backend.student;
+
+public class Student {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final Gender gender;
+}
