68d0ea71f2c6e0c33a7d30485149405d8f62135f
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-15 15:43:34 +0100

    add pom.xml file for spring backend

---
+++ backend/src/test/java/com/mohibullahkamal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohibullahkamal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
