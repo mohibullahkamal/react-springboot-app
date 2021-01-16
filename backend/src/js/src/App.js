961e23087e525bdc266113d46714cb4512cfaa53
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-16 11:51:07 +0100

    use proxy for localhost in package.json; to reduce writing the whole url

--- backend/src/js/src/App.js
+++ backend/src/js/src/App.js
@@ -1,16 +1,30 @@
-// import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
 class App extends Component {
+
+   state = {
+      students: []
+   }
+
+   fetchStudents = () => {
+      getAllStudents()
+         .then(res => res.json()
+         .then(students => {
+            this.setState({
+               students
+            })
+         }
+      ));
+   }
+
 // function App() {
    render() {
-      getAllStudents()
-      .then(res => res.json()
-      .then(students => console.log(students)
-      ))
-
+      // getAllStudents()
+      // .then(res => res.json()
+      // .then(students => console.log(students)
+      // ));
       return <h1>Hello World Spring Boot and React!!</h1>
    }
 }
--- backend/src/js/src/client.js
+++ backend/src/js/src/client.js
@@ -1,3 +1,5 @@
 import fetch from 'unfetch';
 
-export const getAllStudents = () => fetch('http://localhost:8080/students');
+// export const getAllStudents = () => fetch('http://localhost:8080/students');
+
+export const getAllStudents = () => fetch('/students');   //added proxy in package.json... therefore did not add "http://localhost:8080"
