da50d06eb45fceb6884795bca4d27f4db559ae52
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-16 23:55:44 +0100

    regular commit

--- backend/src/main/java/com/mohibullahkamal/backend/student/StudentController.java
+++ backend/src/main/java/com/mohibullahkamal/backend/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.mohibullahkamal.backend.student;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 @RequestMapping("students")
 public class StudentController {
 
