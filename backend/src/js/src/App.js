fc6ca18a96f985c3d0693657319a3bc998489f3b
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-18 10:04:48 +0100

    routine commit

--- backend/src/js/src/App.js
+++ backend/src/js/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
+import { Home } from './home';
 
 class App extends Component {
 
+   num = 10;
    state = { students: [] }
 
    componentDidMount () { this.fetchStudents(); }
@@ -22,7 +24,13 @@ class App extends Component {
          stds.map((std, id) => {
             return ( <div> <h1>{std.studentId}</h1> </div> )
       })}
-      return <h1>Students</h1> }
+      return (
+         <div>
+            <Home num={this.num}></Home>
+            <h1>Students</h1>
+         </div>
+      )
+   }
 }
 
 export default App;
---
+++ backend/src/js/src/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home(props) {
+  return (
+    <div>
+      Hello from HOME tab!! {props.num}
+    </div>
+  )
+}
