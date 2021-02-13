a72171ab70d13870977782ca5f25884fec86a047
mogit2020 <mohibullah.kamal@gmail.com>
2021-02-13 09:57:08 +0100

    random commit

--- backend/src/js/src/App.js
+++ backend/src/js/src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getAllStudents } from './client';
-import { Home } from './home';
+// import React, { Component } from 'react';
+// import './App.css';
+// import { getAllStudents } from './client';
+// import { Home } from './home';
 
-class App extends Component {
+// class App extends Component {
 
-   num = 10;
-   state = { students: [] }
+//    num = 10;
+//    state = { students: [] }
 
-   componentDidMount () { this.fetchStudents(); }
+//    componentDidMount () { this.fetchStudents(); }
 
-   fetchStudents = () => {
-      getAllStudents()
-         .then(res => res.json()
-         .then(stds => {
-            console.log(stds)
-            this.setState({ stds }); }));
-   }
+//    fetchStudents = () => {
+//       getAllStudents()
+//          .then(res => res.json()
+//          .then(stds => {
+//             console.log(stds)
+//             this.setState({ stds }); }));
+//    }
 
-   render() {
-      const { stds } = this.props;
-      if (stds && stds.length) {
-         stds.map((std, id) => {
-            return ( <div> <h1>{std.studentId}</h1> </div> )
-      })}
-      return (
-         <div>
-            <Home num={this.num}></Home>
-            <h1>Students</h1>
-         </div>
-      )
-   }
-}
+//    render() {
+//       const { stds } = this.props;
+//       if (stds && stds.length) {
+//          stds.map((std, id) => {
+//             return ( <div> <h1>{std.studentId}</h1> </div> )
+//       })}
+//       return (
+//          <div>
+//             <Home num={this.num}></Home>
+//             <h1>Students</h1>
+//          </div>
+//       )
+//    }
+// }
 
-export default App;
+// export default App;
 
 
 
