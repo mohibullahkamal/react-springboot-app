3bcd27f2b901c1f295e9d91014dbc1a511f7859a
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2021-03-17 22:46:41 +0100

    Update Student.java

--- backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
+++ backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
@@ -158,32 +158,3 @@ public class Student {
 //         return gender;
 //     }
 
-
-
-//     enum Gender {
-//         MALE, FEMALE
-//     }
-// }package com.mohibullahkamal.backend.student;
-
-// import java.util.UUID;
-
-// public class Student {
-//     private final UUID studentId;
-//     private final String firstName;
-//     private final String lastName;
-//     private final String email;
-//     private final Gender gender;
-
-//     public Student(UUID studentId,
-//                    String firstName,
-//                    String lastName,
-//                    String email,
-//                    Gender gender) {
-//         this.studentId = studentId;
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.email = email;
-//         this.gender = gender;
-//     }
-
-
