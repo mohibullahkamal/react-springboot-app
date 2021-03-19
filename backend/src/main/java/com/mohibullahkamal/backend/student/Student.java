fb63aa2fcf9c4b12dc6882ddc9e9cba9e6b15d1c
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2021-03-19 09:01:37 +0100

    Update Student.java

--- backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
+++ backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
@@ -255,3 +255,79 @@ public class Student {
 //     public Gender getGender() {
 //         return gender;
 //     }
+
+
+
+
+
+
+
+//     public UUID getStudentId() {
+//         return studentId;
+//     }
+
+//     public String getFirstName() {
+//         return firstName;
+//     }
+
+//     public String getLastName() {
+//         return lastName;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public Gender getGender() {
+//         return gender;
+//     }
+
+
+
+
+
+
+// package com.mohibullahkamal.backend.student;
+
+// import java.util.UUID;
+
+// public class Student {
+//     private final UUID studentId;
+//     private final String firstName;
+//     private final String lastName;
+//     private final String email;
+//     private final Gender gender;
+
+//     public Student(UUID studentId,
+//                    String firstName,
+//                    String lastName,
+//                    String email,
+//                    Gender gender) {
+//         this.studentId = studentId;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.email = email;
+//         this.gender = gender;
+//     }
+
+
+
+//     public UUID getStudentId() {
+//         return studentId;
+//     }
+
+//     public String getFirstName() {
+//         return firstName;
+//     }
+
+//     public String getLastName() {
+//         return lastName;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public Gender getGender() {
+//         return gender;
+//     }
