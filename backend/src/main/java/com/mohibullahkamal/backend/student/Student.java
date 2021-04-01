68f9f62c7b21be03897065c0bf61be4cd76a417a
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2021-04-01 08:05:34 +0200

    Update Student.java

--- backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
+++ backend/src/main/java/com/mohibullahkamal/backend/student/Student.java
@@ -340,30 +340,3 @@ public class Student {
 
 
 
-
-
-// package com.mohibullahkamal.backend.student;
-
-// import java.util.UUID;
-
-// public class Student {
-//     private final UUID studentId;
-//     private final String firstName;
-//     private final String lastName;
-//     private final String email;
-//     private final Gender gender;
-
-//     public Student(UUID studentId,
-//                    String firstName,
-//                    String lastName,
-//                    String email,
-//                    Gender gender) {
-//         this.studentId = studentId;
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.email = email;
-//         this.gender = gender;
-//     }
-
-
-
